596f978a6c3c1c321e784e1fbb9c10b51548e977
Faizy Ahsan <faizy.ahsan@mail.mcgill.ca>
2019-09-03 12:28:00 -0400

    m

--- Predict_ortho.py
+++ Predict_ortho.py
@@ -1,5 +1,22 @@
 import argparse
 import numpy as np
+import tensorflow as tf
+from Model.Legacy_RNATracker import RNATracker
+
+
+BATCH_SIZE = 128
+EPOCHS = 50
+DEVICES = ['/gpu:0']
+MAX_LEN = 100
+
+hp = {
+    'learning_rate': 2e-4,
+    'dropout_rate': 0.2,
+    'use_clr': True,
+    'units': 128,
+    'use_bn': False,
+}
+
 
 def load_seq(fname):
     '''
@@ -26,16 +43,13 @@ if __name__ == '__main__':
                         help="path to ortho validation filename. Default: None")
     parser.add_argument("-test", "--test-name", default=None, type=str,
                         help="path to ortho test filename. Default: None")
-    parser.add_argument("--model-name", default=None, type=str,
-                        help="path to saved model. Default: None")
+    parser.add_argument("--model-weights", default=None, type=str,
+                        help="path to saved model weights. Default: None")
 
     args = parser.parse_args()
 
     # load train ortho
     train_headers, train_species, train_seq, train_y = load_seq(args.train_name)
-
-    # print ('train_headers:', train_headers[:10], 'train_species:', train_species[:10],
-    #        'train_seq:', train_seq[:10], 'train_y:', train_y[:10])
 
     # load validation ortho
     val_headers, val_species, val_seq, val_y = load_seq(args.validation_name)
@@ -48,9 +62,26 @@ if __name__ == '__main__':
     VOCAB = ['NOT_FOUND', 'A', 'C', 'G', 'T', 'N']
     VOCAB_VEC = np.array([[0, 0, 0, 0, 0], [1, 0, 0, 0, 0], [0, 1, 0, 0, 0], [0, 0, 1, 0, 0], [0, 0, 0, 1, 0],
                           [0, 0, 0, 0, 1]]).astype(np.float32)
-    #TODO: Zichao
+    model = RNATracker(MAX_LEN, VOCAB_VEC.shape[1], DEVICES, **hp)
+    model.load(tf.train.latest_checkpoint(args.model_weights))
 
-    # predict on sequences
-    #TODO: Zichao
+    # predict on train ortho
+    train_preds = model.predict(train_seq, BATCH_SIZE)
+
+    # predict on validation ortho
+    val_preds = model.predict(val_seq, BATCH_SIZE)
+
+    # predict on test ortho
+    test_preds = model.predict(test_seq, BATCH_SIZE)
 
     # save predictions as df
+    
+
+
+
+
+
+
+
+
+
