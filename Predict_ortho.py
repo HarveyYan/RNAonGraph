f45585671674a90a39ee6a0a77190d51f62a3d3b
Faizy Ahsan <faizy.ahsan@mail.mcgill.ca>
2019-09-03 14:08:34 -0400

    m

--- Predict_ortho.py
+++ Predict_ortho.py
@@ -121,11 +121,11 @@ if __name__ == '__main__':
     print ('df_train:', df_train.shape)
     pickle.dump(df_train, open(args.save_path+'/df_train.pkl', 'wb'))
 
-    df_validate = pd.DataFrame(val_preds, columns=sp_list + ['y'])
+    df_validate = create_df(val_headers, val_species, val_y, val_preds)
     pickle.dump(df_validate, open(args.save_path+'/df_validate.pkl', 'wb'))
     print ('df_validate:', df_validate.shape)
 
-    df_test = pd.DataFrame(test_preds, columns=sp_list + ['y'])
+    df_test = create_df(test_headers, test_species, test_y, test_preds)
     pickle.dump(df_test, open(args.save_path+'/df_test.pkl', 'wb'))
     print ('df_test:', df_test.shape)
 
