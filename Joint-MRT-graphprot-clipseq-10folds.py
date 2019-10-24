integrated gradients and a lot of bug fixes

Joint-MRT-graphprot-clipseq-10folds.py
@@ -64,7 +64,8 @@ def Logger(q):
     all_auc = []
     registered_gpus = {}
     logger = lib.logger.CSVLogger('results.csv', output_dir,
-                                  ['fold', 'seq_acc', 'nuc_acc', 'auc'])
+                                  ['fold', 'seq_acc', 'nuc_acc', 'auc',
+                                   'original_seq_acc', 'original_nuc_acc', 'original_auc'])
     while True:
         msg = q.get()
         print(msg)
@@ -106,7 +107,7 @@ def run_one_rbp(idx, q):
     fold_output = os.path.join(output_dir, 'fold%d' % (idx))
     os.makedirs(fold_output)
 
-    outfile = open(os.path.join(fold_output, str(os.getpid())) + ".out", "w", buffering=0)
+    outfile = open(os.path.join(fold_output, str(os.getpid())) + ".out", "w")
     sys.stdout = outfile
     sys.stderr = outfile
 
@@ -135,7 +136,24 @@ def run_one_rbp(idx, q):
 
     test_data = [dataset['seq'][test_idx], dataset['segment_size'][test_idx], dataset['raw_seq'][test_idx]]
     cost, acc, auc = model.evaluate(test_data, dataset['label'][test_idx], BATCH_SIZE, random_crop=False)
-    print('Evaluation (with masking) on held-out test set, acc: %s, auc: %.3f' % (acc, auc))
+    print('Evaluation (with masking) on modified held-out test set, acc: %s, auc: %.3f' % (acc, auc))
+
+    original_test_data = [original_dataset['seq'][test_idx], original_dataset['segment_size'][test_idx],
+                 original_dataset['raw_seq'][test_idx]]
+    original_cost, original_acc, original_auc = model.evaluate(test_data, original_dataset['label'][test_idx],
+                                                               BATCH_SIZE, random_crop=False)
+    print('Evaluation (with masking) on original held-out test set, acc: %s, auc: %.3f' % (acc, auc))
+
+    # get predictions
+    np.save(os.path.join(fold_output, 'predictions.npy'), model.predict(original_test_data, BATCH_SIZE))
+
+    # plot some motifs
+    graph_dir = os.path.join(fold_output, 'integrated_gradients')
+    if not os.path.exists(graph_dir):
+        os.makedirs(graph_dir)
+
+    model.integrated_gradients(original_test_data, original_dataset['label'][test_idx],
+                               original_dataset['id'][test_idx], save_path=graph_dir, max_plots=200)
 
     model.delete()
     reload(lib.plot)
@@ -144,7 +162,10 @@ def run_one_rbp(idx, q):
         'fold': idx,
         'seq_acc': acc[0],
         'nuc_acc': acc[1],
-        'auc': auc
+        'auc': auc,
+        'original_seq_acc': original_acc[0],
+        'original_nuc_acc': original_acc[1],
+        'original_auc': original_auc
     })
 
 
@@ -173,7 +194,13 @@ if __name__ == "__main__":
     dataset = \
         lib.graphprot_dataloader.load_clip_seq(
             [TRAIN_RBP_ID], use_embedding=FLAGS.use_embedding,
-            load_mat=False, nucleotide_label=True, modify_leaks=True)[0]  # load one at a time
+            load_mat=False, nucleotide_label=True, modify_leaks=True)[0]
+
+    original_dataset = \
+        lib.graphprot_dataloader.load_clip_seq(
+            [TRAIN_RBP_ID], use_embedding=FLAGS.use_embedding,
+            load_mat=False, nucleotide_label=True, modify_leaks=False)[0]
+
     np.save(os.path.join(output_dir, 'splits.npy'), dataset['splits'])
     manager = mp.Manager()
     q = manager.Queue()

Joint-RGCN-graphprot-clipseq-10folds.py
@@ -83,8 +83,9 @@ def Logger(q):
     all_auc = []
     registered_gpus = {}
     logger = lib.logger.CSVLogger('results.csv', output_dir,
-                                  ['fold', 'seq_acc', 'gnn_pos_acc', 'bilstm_pos_acc',
-                                   'gnn_nuc_acc', 'bilstm_nuc_acc', 'auc'])
+                                  ['fold', 'seq_acc', 'gnn_nuc_acc', 'bilstm_nuc_acc', 'auc',
+                                   'original_seq_acc', 'original_gnn_nuc_acc', 'original_bilstm_nuc_acc',
+                                   'original_auc'])
     while True:
         msg = q.get()
         print(msg)
@@ -156,12 +157,31 @@ def run_one_rbp(idx, q):
                   dataset['segment_size'][train_idx], dataset['raw_seq'][train_idx]]
     model.fit(train_data, dataset['label'][train_idx], EPOCHS, BATCH_SIZE, fold_output, logging=True)
 
+    # evaluate our model on the debiased test data
     test_data = [dataset['seq'][test_idx], dataset['all_data'][test_idx], dataset['all_row_col'][test_idx],
                  dataset['segment_size'][test_idx], dataset['raw_seq'][test_idx]]
-
     cost, acc, auc = model.evaluate(test_data, dataset['label'][test_idx], BATCH_SIZE, random_crop=False)
+    print('Evaluation (with masking) on debiased held-out test set, acc (pos, seq): %s, auc: %.3f' % (acc, auc))
 
-    print('Evaluation (with masking) on held-out test set, acc (pos, seq): %s, auc: %.3f' % (acc, auc))
+    # evaluate our model on the original data with the bias
+    original_test_data = [original_dataset['seq'][test_idx], original_dataset['all_data'][test_idx],
+                          original_dataset['all_row_col'][test_idx], original_dataset['segment_size'][test_idx],
+                          original_dataset['raw_seq'][test_idx]]
+    original_cost, original_acc, original_auc = \
+        model.evaluate(original_test_data, original_dataset['label'][test_idx], BATCH_SIZE, random_crop=False)
+    print('Evaluation (with masking) on original held-out test set, acc (pos, seq): %s, auc: %.3f' %
+          (original_acc, original_auc))
+
+    # get predictions
+    np.save(os.path.join(fold_output, 'predictions.npy'), model.predict(original_test_data, BATCH_SIZE))
+
+    # plot some motifs
+    graph_dir = os.path.join(fold_output, 'integrated_gradients')
+    if not os.path.exists(graph_dir):
+        os.makedirs(graph_dir)
+
+    model.integrated_gradients(original_test_data, original_dataset['label'][test_idx],
+                               original_dataset['id'][test_idx], save_path=graph_dir, max_plots=200)
 
     model.delete()
     reload(lib.plot)
@@ -171,7 +191,11 @@ def run_one_rbp(idx, q):
         'seq_acc': acc[0],
         'gnn_nuc_acc': acc[1],
         'bilstm_nuc_acc': acc[2],
-        'auc': auc
+        'auc': auc,
+        'original_seq_acc': original_acc[0],
+        'original_gnn_nuc_acc': original_acc[1],
+        'original_bilstm_nuc_acc': original_acc[2],
+        'original_auc': original_auc,
     })
 
 
@@ -197,11 +221,20 @@ if __name__ == "__main__":
     shutil.copy(inspect.getfile(lib.graphprot_dataloader), backup_dir)
     shutil.copy(inspect.getfile(lib.rna_utils), backup_dir)
 
+    # This one corrects the nucleotide bias at the beginning or at the end of a viewpoint region
     dataset = \
         lib.graphprot_dataloader.load_clip_seq([TRAIN_RBP_ID], use_embedding=FLAGS.use_embedding,
                                                fold_algo=FLAGS.fold_algo,
                                                probabilistic=FLAGS.probabilistic, w=FLAGS.folding_winsize,
-                                               nucleotide_label=True, modify_leaks=True)[0]  # load one at a time
+                                               nucleotide_label=True, modify_leaks=True)[0]
+
+    # This one is the original dataset, with the bias
+    original_dataset = \
+        lib.graphprot_dataloader.load_clip_seq([TRAIN_RBP_ID], use_embedding=FLAGS.use_embedding,
+                                               fold_algo=FLAGS.fold_algo,
+                                               probabilistic=FLAGS.probabilistic, w=FLAGS.folding_winsize,
+                                               nucleotide_label=True, modify_leaks=False)[0]  # load one at a time
+
     np.save(os.path.join(output_dir, 'splits.npy'), dataset['splits'])
     manager = mp.Manager()
     q = manager.Queue()

Model/Joint_MRT.py
@@ -62,6 +62,7 @@ class JMRT:
                 self.saver = tf.train.Saver(max_to_keep=10)
                 self.init = tf.global_variables_initializer()
                 self.local_init = tf.local_variables_initializer()
+                self.g.finalize()
         self._init_session()
 
     def _placeholders(self):
@@ -196,9 +197,11 @@ class JMRT:
             predictions=self.prediction[:, 1],
         )
 
+        self.g_nodes = tf.gradients(self.prediction[:, 1], self.node_tensor)[0]
+
     def _init_session(self):
         gpu_options = tf.GPUOptions()
-        gpu_options.allow_growth = True
+        gpu_options.per_process_gpu_memory_fraction = 0.45
         if type(self.gpu_device) is list:
             gpu_options.visible_device_list = ','.join([device[-1] for device in self.gpu_device])
         else:
@@ -252,7 +255,7 @@ class JMRT:
 
         return np.array(m_seq), np.array(m_sg), np.array(m_label)
 
-    def fit(self, X, y, epochs, batch_size, output_dir, logging=False, epoch_to_start=0):
+    def fit(self, X, y, epochs, batch_size, output_dir, logging=False, epoch_to_start=0, random_crop=False):
         checkpoints_dir = os.path.join(output_dir, 'checkpoints/')
         if not os.path.exists(checkpoints_dir):
             os.makedirs(checkpoints_dir)
@@ -286,9 +289,10 @@ class JMRT:
             node_tensor, segment_length, raw_seq = self.indexing_iterable(X, permute)
             y = train_targets[permute]
 
-            # augmentation
-            # node_tensor, segment_length, y = \
-            #     self.random_crop(node_tensor, raw_seq, y)
+            if random_crop:
+                # augmentation
+                node_tensor, segment_length, y = \
+                    self.random_crop(node_tensor, raw_seq, y)
 
             prepro_time = 0.
             training_time = 0.
@@ -356,7 +360,7 @@ class JMRT:
         if logging:
             logger.close()
 
-    def evaluate(self, X, y, batch_size, random_crop=True):
+    def evaluate(self, X, y, batch_size, random_crop=False):
         node_tensor, segment_length, raw_seq = X
         if random_crop:
             # augmentation
@@ -394,37 +398,67 @@ class JMRT:
         return (all_cost / len(node_tensor), all_graph_cost / len(node_tensor),
                 all_bilstm_nuc_cost / len(node_tensor)), acc, auc
 
-    def predict(self, X, y=None):
-        # predict one at a time without masking
-        node_tensor, segment_length = X
-        all_predicton = []
-        for i in range(len(node_tensor)):
+    def predict(self, X, batch_size):
+        node_tensor, segment_length, raw_seq = X
+        preds = []
+        iters_per_epoch = len(node_tensor) // batch_size + (0 if len(node_tensor) % batch_size == 0 else 1)
+        for i in range(iters_per_epoch):
             _node_tensor, _segment \
-                = node_tensor[i], \
-                  segment_length[i]
+                = node_tensor[i * batch_size: (i + 1) * batch_size], \
+                  segment_length[i * batch_size: (i + 1) * batch_size]
+
+            _max_len = max(_segment)
 
             feed_dict = {
-                self.node_input_ph: _node_tensor,
+                self.node_input_ph: np.concatenate(_node_tensor, axis=0),
+                self.max_len: _max_len,
+                self.segment_length: _segment,
+                self.is_training_ph: False
+            }
+            preds.append(self.sess.run(self.prediction, feed_dict))
+
+        return np.concatenate(np.array(preds), axis=0)
+
+    def integrated_gradients(self, X, y, ids, interp_steps=100, save_path=None, max_plots=np.inf):
+        counter = 0
+        print('begins')
+        for _node_tensor, _segment, _, _label, _id in zip(*X, y, ids):
+            if np.max(_label) == 0:
+                continue
+            if counter >= max_plots:
+                break
+            _meshed_node_tensor = np.array([self.embedding_vec[idx] for idx in _node_tensor])
+            _meshed_reference_input = np.zeros_like(_meshed_node_tensor)
+            new_node_tensor = []
+            for i in range(0, interp_steps + 1):
+                new_node_tensor.append(
+                    _meshed_reference_input + i / interp_steps * (_meshed_node_tensor - _meshed_reference_input))
+
+            feed_dict = {
+                self.node_tensor: np.concatenate(np.array(new_node_tensor), axis=0),
                 self.max_len: _segment,
-                self.segment_length: [_segment],
+                self.segment_length: [_segment] * (interp_steps + 1),
                 self.is_training_ph: False
             }
 
-            feed_tensor = [self.prediction]
+            grads = self.sess.run(self.g_nodes, feed_dict).reshape((interp_steps + 1, _segment, 4))
+            grads = (grads[:-1] + grads[1:]) / 2.0
+            node_scores = np.average(grads, axis=0) * (_meshed_node_tensor - _meshed_reference_input)
 
-            if y is not None:
-                feed_dict[self.labels] = [y[i]]
-                feed_tensor += [self.acc_update_op, self.auc_update_op]
+            pos_idx = np.where(_label == 1)[0]
+            extended_start = max(pos_idx[0] - 50, 0)
+            extended_end = min(pos_idx[-1] + 50, _segment)
+            extended_region = [extended_start, extended_end]
+            viewpoint_region = [pos_idx[0] - extended_start, pos_idx[-1] - extended_start + 1]
 
-            all_predicton.append(self.sess.run(feed_tensor, feed_dict)[0])
-        all_predicton = np.concatenate(all_predicton, axis=0)
-
-        if y is not None:
-            acc, auc = self.sess.run([self.acc_val, self.auc_val])
-            self.sess.run(self.local_init)
-            return all_predicton, acc, auc
-        else:
-            return all_predicton
+            if save_path is not None:
+                saveto = os.path.join(save_path, '%s.jpg' % (_id))
+            else:
+                saveto = None
+            lib.plot.plot_weights(node_scores[range(*extended_region)],
+                                  subticks_frequency=10, highlight={'r': [viewpoint_region]},
+                                  save_path=saveto)
+            counter += 1
 
     def delete(self):
         tf.reset_default_graph()

Model/Joint_SMRGCN.py
@@ -307,9 +307,11 @@ class JSMRGCN:
             predictions=self.prediction[:, 1],
         )
 
+        self.g_nodes = tf.gradients(self.prediction[:, 1], self.node_tensor)[0]
+
     def _init_session(self):
         gpu_options = tf.GPUOptions()
-        gpu_options.allow_growth = True
+        gpu_options.per_process_gpu_memory_fraction = 0.45
         if type(self.gpu_device) is list:
             gpu_options.visible_device_list = ','.join([device[-1] for device in self.gpu_device])
         else:
@@ -521,6 +523,8 @@ class JSMRGCN:
             logger.close()
         train_generator.kill.set()
         val_generator.kill.set()
+        train_generator.join(1)
+        val_generator.join(1)
 
     def evaluate_with_generator(self, generator):
         all_cost, all_graph_cost, all_gnn_nuc_cost, all_bilstm_nuc_cost = 0., 0., 0., 0.
@@ -587,42 +591,74 @@ class JSMRGCN:
         return (all_cost / len(node_tensor), all_graph_cost / len(node_tensor),
                 all_gnn_nuc_cost / len(node_tensor), all_bilstm_nuc_cost / len(node_tensor)), acc, auc
 
-    def predict(self, X, y=None):
-        # predict one at a time without masking
-        node_tensor, all_rel_data, all_row_col, segment_length = X
-        all_predicton = []
-        for i in range(len(node_tensor)):
+    def predict(self, X, batch_size):
+        node_tensor, all_rel_data, all_row_col, segment_length, raw_seq = X
+        preds = []
+        iters_per_epoch = len(node_tensor) // batch_size + (0 if len(node_tensor) % batch_size == 0 else 1)
+        for i in range(iters_per_epoch):
             _node_tensor, _rel_data, _row_col, _segment \
-                = node_tensor[i], \
-                  all_rel_data[i], \
-                  all_row_col[i], \
-                  segment_length[i]
+                = node_tensor[i * batch_size: (i + 1) * batch_size], \
+                  all_rel_data[i * batch_size: (i + 1) * batch_size], \
+                  all_row_col[i * batch_size: (i + 1) * batch_size], \
+                  segment_length[i * batch_size: (i + 1) * batch_size], \
 
-            all_adj_mat = self._merge_sparse_submatrices([_rel_data], [_row_col], [_segment])
+            _max_len = max(_segment)
+            all_adj_mat = self._merge_sparse_submatrices(_rel_data, _row_col, _segment)
 
             feed_dict = {
-                self.node_input_ph: _node_tensor,
+                self.node_input_ph: np.concatenate(_node_tensor, axis=0),
+                **{self.adj_mat_ph[i]: all_adj_mat[i] for i in range(4)},
+                self.max_len: _max_len,
+                self.segment_length: _segment,
+                self.is_training_ph: False
+            }
+            preds.append(self.sess.run(self.prediction, feed_dict))
+
+        return np.concatenate(np.array(preds), axis=0)
+
+    def integrated_gradients(self, X, y, ids, interp_steps=100, save_path=None, max_plots=np.inf):
+        counter = 0
+        for _node_tensor, _rel_data, _row_col, _segment, _, _label, _id in zip(*X, y, ids):
+            if np.max(_label) == 0:
+                continue
+            if counter >= max_plots:
+                break
+            _meshed_node_tensor = np.array([self.embedding_vec[idx] for idx in _node_tensor])
+            _meshed_reference_input = np.zeros_like(_meshed_node_tensor)
+            new_node_tensor = []
+            for i in range(0, interp_steps + 1):
+                new_node_tensor.append(
+                    _meshed_reference_input + i / interp_steps * (_meshed_node_tensor - _meshed_reference_input))
+            all_adj_mat = self._merge_sparse_submatrices([_rel_data] * (interp_steps + 1),
+                                                         [_row_col] * (interp_steps + 1),
+                                                         [_segment] * (interp_steps + 1))
+
+            feed_dict = {
+                self.node_tensor: np.concatenate(np.array(new_node_tensor), axis=0),
                 **{self.adj_mat_ph[i]: all_adj_mat[i] for i in range(4)},
                 self.max_len: _segment,
-                self.segment_length: [_segment],
+                self.segment_length: [_segment] * (interp_steps + 1),
                 self.is_training_ph: False
             }
 
-            feed_tensor = [self.prediction]
+            grads = self.sess.run(self.g_nodes, feed_dict).reshape((interp_steps + 1, _segment, 4))
+            grads = (grads[:-1] + grads[1:]) / 2.0
+            node_scores = np.average(grads, axis=0) * (_meshed_node_tensor - _meshed_reference_input)
 
-            if y is not None:
-                feed_dict[self.labels] = [y[i]]
-                feed_tensor += [self.acc_update_op, self.auc_update_op]
+            pos_idx = np.where(_label == 1)[0]
+            extended_start = max(pos_idx[0] - 50, 0)
+            extended_end = min(pos_idx[-1] + 50, _segment)
+            extended_region = [extended_start, extended_end]
+            viewpoint_region = [pos_idx[0] - extended_start, pos_idx[-1] - extended_start + 1]
 
-            all_predicton.append(self.sess.run(feed_tensor, feed_dict)[0])
-        all_predicton = np.concatenate(all_predicton, axis=0)
-
-        if y is not None:
-            acc, auc = self.sess.run([self.acc_val, self.auc_val])
-            self.sess.run(self.local_init)
-            return all_predicton, acc, auc
-        else:
-            return all_predicton
+            if save_path is not None:
+                saveto = os.path.join(save_path, '%s.jpg' % (_id))
+            else:
+                saveto = None
+            lib.plot.plot_weights(node_scores[range(*extended_region)],
+                                  subticks_frequency=10, highlight={'r': [viewpoint_region]},
+                                  save_path=saveto)
+            counter += 1
 
     def delete(self):
         tf.reset_default_graph()

lib/graphprot_dataloader.py
@@ -132,12 +132,20 @@ def load_clip_seq(rbp_list=None, p=None, **kwargs):
                     else:
                         pos_idx = np.where((np.array(seq) <= 'Z').astype(np.int32) == 1)[0]
 
-                    if rbp == 'CAPRIN1_Baltz2012':
-                        indices = [pos_idx[-1], pos_idx[0] + 1, pos_idx[0], pos_idx[0] - 1]
+                    if rbp == 'PARCLIP_IGF2BP123':
+                        indices = [pos_idx[-1], pos_idx[0], pos_idx[0] - 1, pos_idx[0] - 2]
                     elif rbp == 'CAPRIN1_Baltz2012':
-                        indices = [pos_idx[-1], pos_idx[-1] - 1, pos_idx[0] + 1, pos_idx[0], pos_idx[0] - 1]
+                        indices = [pos_idx[-1], pos_idx[-1] - 1, pos_idx[0], pos_idx[0] - 1, pos_idx[0] - 2]
                     elif rbp == 'PARCLIP_PUM2':
+                        # unable to obtain perfect mask
                         indices = [pos_idx[-1], pos_idx[0] - 1]
+                    elif rbp == 'PARCLIP_AGO1234':
+                        # unable to obtain perfect mask
+                        indices = [pos_idx[-1], pos_idx[0] - 1]
+                    elif rbp == 'PARCLIP_MOV10_Sievers':
+                        indices = [pos_idx[-1], pos_idx[-1] - 1, pos_idx[0] - 1]
+                    elif rbp == 'ZC3H7B_Baltz2012':
+                        indices = [pos_idx[-1], pos_idx[-1] - 1, pos_idx[0] + 1, pos_idx[0], pos_idx[0] - 1]
                     else:
                         raise ValueError('TODO: modify_leaks option has not been made available for %s' % (rbp))
 